f7f8b1ea967a72e4fd898f35300f723be7bcef76
Matt Hodges <hodgesmr1@gmail.com>
2022-12-01 18:52:15 -0600

    2 scoring frameworks and 2 weight frameworks

--- scorers.py
+++ scorers.py
@@ -10,6 +10,32 @@ if TYPE_CHECKING:
     from models import ScoredPost
 
 
+class Weight(ABC):
+    @classmethod
+    @abstractmethod
+    def weight(cls, scored_post: ScoredPost):
+        pass
+
+
+class UniformWeight(Weight):
+    @classmethod
+    def weight(cls, scored_post: ScoredPost):
+        return 1
+
+
+class InverseFollowerWeight(Weight):
+    @classmethod
+    def weight(cls, scored_post: ScoredPost):
+        # Zero out posts by accounts with zero followers that somehow made it to my feed
+        if scored_post.info["account"]["followers_count"] == 0:
+            weight = 0
+        else:
+            # inversely weight against how big the account is
+            weight = 1 / sqrt(scored_post.info["account"]["followers_count"])
+
+        return weight
+
+
 class Scorer(ABC):
     @classmethod
     @abstractmethod
@@ -17,19 +43,38 @@ class Scorer(ABC):
         pass
 
 
-class SimpleScorer(Scorer):
+class SimpleScorer(Scorer, UniformWeight):
     @classmethod
     def score(cls, scored_post: ScoredPost):
         metric_average = stats.gmean(
-            [scored_post.info["reblogs_count"], scored_post.info["favourites_count"]]
+            [
+                scored_post.info["reblogs_count"],
+                scored_post.info["favourites_count"],
+            ]
         )
-        return metric_average
+        return metric_average * super().weight(scored_post)
 
 
-class SimpleWeightedScorer(SimpleScorer):
+class SimpleWeightedScorer(SimpleScorer, InverseFollowerWeight):
     @classmethod
     def score(cls, scored_post: ScoredPost):
-        unweighted_score = super().score(scored_post)
-        weight = 1 / sqrt(scored_post.info["account"]["followers_count"])
+        return super().score(scored_post) * super().weight(scored_post)
 
-        return unweighted_score * weight
+
+class ExtendedSimpleScorer(Scorer, UniformWeight):
+    @classmethod
+    def score(cls, scored_post: ScoredPost):
+        metric_average = stats.gmean(
+            [
+                scored_post.info["reblogs_count"],
+                scored_post.info["favourites_count"],
+                scored_post.info["replies_count"],
+            ],
+        )
+        return metric_average * super().weight(scored_post)
+
+
+class ExtendedSimpleWeightedScorer(ExtendedSimpleScorer, InverseFollowerWeight):
+    @classmethod
+    def score(cls, scored_post: ScoredPost):
+        return super().score(scored_post) * super().weight(scored_post)
