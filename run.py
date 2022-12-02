8c3b0699cd5997b38f98d968b7caf206d6783b20
Matt Hodges <hodgesmr1@gmail.com>
2022-12-02 16:28:22 -0600

    some comments and cleanup

--- run.py
+++ run.py
@@ -1,73 +1,22 @@
+from __future__ import annotations
+
 import argparse
 import os
 import sys
 import tempfile
 import webbrowser
-from datetime import datetime, timedelta, timezone
+from typing import TYPE_CHECKING
 
 from jinja2 import Environment, FileSystemLoader
 from mastodon import Mastodon
-from scipy import stats
 
-from models import ScoredPost
-from scorers import Scorer, get_scorers
-from thresholds import Threshold, get_thresholds, get_threshold_from_name
+from api import fetch_posts_and_boosts
+from scorers import get_scorers
+from thresholds import get_threshold_from_name, get_thresholds
 
-
-def fetch_posts_and_boosts(
-    hours: int, mastodon_client: Mastodon, mastodon_username: str
-) -> tuple[list[ScoredPost], list[ScoredPost]]:
-    TIMELINE_LIMIT = 1000
-
-    # First, get our filters
-    filters = mastodon_client.filters()
-
-    # Set our start query
-    start = datetime.now(timezone.utc) - timedelta(hours=hours)
-
-    posts = []
-    boosts = []
-    seen_post_urls = set()
-    total_posts_seen = 0
-
-    # Iterate over our home timeline until we run out of posts or we hit the limit
-    response = mastodon_client.timeline(min_id=start)
-    while response and total_posts_seen < TIMELINE_LIMIT:
-
-        # Apply our server-side filters
-        filtered_response = mastodon_client.filters_apply(response, filters, "home")
-
-        for post in filtered_response:
-            total_posts_seen += 1
-
-            boost = False
-            if post["reblog"] is not None:
-                post = post["reblog"]  # look at the bosted post
-                boost = True
-
-            scored_post = ScoredPost(post)  # wrap the post data as a ScoredPost
-
-            if scored_post.url not in seen_post_urls:
-                # Apply our local filters
-                # Basically ignore my posts or posts I've interacted with
-                if (
-                    not scored_post.info["reblogged"]
-                    and not scored_post.info["favourited"]
-                    and not scored_post.info["bookmarked"]
-                    and scored_post.info["account"]["acct"] != mastodon_username
-                ):
-                    # Append to either the boosts list or the posts lists
-                    if boost:
-                        boosts.append(scored_post)
-                    else:
-                        posts.append(scored_post)
-                    seen_post_urls.add(scored_post.url)
-
-        response = mastodon_client.fetch_previous(
-            response
-        )  # fext the previous (because of reverse chron) page of results
-
-    return posts, boosts
+if TYPE_CHECKING:
+    from scorers import Scorer
+    from thresholds import Threshold
 
 
 def render_and_open_digest(context: dict) -> None:
@@ -98,20 +47,22 @@ def run(
         api_base_url=mastodon_base_url,
     )
 
+    # 1. Fetch all the posts and boosts from our home timeline that we haven't interacted with
     posts, boosts = fetch_posts_and_boosts(hours, mst, mastodon_username)
 
+    # 2. Score them, and return those that meet our threshold
     threshold_posts = threshold.posts_meeting_criteria(posts, scorer)
     threshold_boosts = threshold.posts_meeting_criteria(boosts, scorer)
 
-
-    digest_context = {
-        "hours": hours,
-        "posts": threshold_posts,
-        "boosts": threshold_boosts,
-        "mastodon_base_url": mastodon_base_url,
-    }
-
-    render_and_open_digest(digest_context)
+    # 3. Build and show the digest
+    render_and_open_digest(
+        context={
+            "hours": hours,
+            "posts": threshold_posts,
+            "boosts": threshold_boosts,
+            "mastodon_base_url": mastodon_base_url,
+        }
+    )
 
 
 if __name__ == "__main__":
--- thresholds.py
+++ thresholds.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
+
 from enum import Enum
+from typing import TYPE_CHECKING
 
 from scipy import stats
 
-from models import ScoredPost
-from scorers import Scorer
+if TYPE_CHECKING:
+    from models import ScoredPost
+    from scorers import Scorer
 
 
 class Threshold(Enum):
@@ -14,20 +18,26 @@ class Threshold(Enum):
     def posts_meeting_criteria(
         self, posts: list[ScoredPost], scorer: Scorer
     ) -> list[ScoredPost]:
+        """Returns a list of ScoredPosts that meet this Threshold with the given Scorer"""
+
         all_post_scores = [p.get_score(scorer) for p in posts]
         threshold_posts = [
             p
             for p in posts
             if stats.percentileofscore(all_post_scores, p.get_score(scorer))
-            > self.value
+            >= self.value
         ]
 
         return threshold_posts
 
 
 def get_thresholds():
+    """Returns a dictionary mapping lowercase threshold names to values"""
+
     return {i.name.lower(): i.value for i in Threshold}
 
 
 def get_threshold_from_name(name: str) -> Threshold:
+    """Returns Threshold for a given named string"""
+
     return Threshold[name.upper()]
