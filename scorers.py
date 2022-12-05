aa85304eb8374bec33c8eb2cca5d5198b0102e3b
Matt Hodges <hodgesmr1@gmail.com>
2022-12-04 18:28:40 -0600

    method to get scorer name

--- scorers.py
+++ scorers.py
@@ -44,6 +44,10 @@ class Scorer(ABC):
     def score(cls, scored_post: ScoredPost):
         pass
 
+    @classmethod
+    def get_name(cls):
+        return cls.__name__.replace("Scorer", "")
+
 
 class SimpleScorer(UniformWeight, Scorer):
     @classmethod
@@ -97,4 +101,4 @@ class ExtendedSimpleWeightedScorer(InverseFollowerWeight, ExtendedSimpleScorer):
 def get_scorers():
     all_classes = inspect.getmembers(importlib.import_module(__name__), inspect.isclass)
     scorers = [c for c in all_classes if c[1] != Scorer and issubclass(c[1], Scorer)]
-    return {scorer[0].replace("Scorer", ""): scorer[1] for scorer in scorers}
+    return {scorer[1].get_name(): scorer[1] for scorer in scorers}
