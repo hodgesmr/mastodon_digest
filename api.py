e84855570244fb600f3d8281df82ff3a8b04e8c6
Richard Martin-Nielsen <richard.martin@alumni.utoronto.ca>
2023-02-07 21:18:14 +0200

    Fixed api call and uncommented code. Appears to work.

--- api.py
+++ api.py
@@ -115,4 +115,5 @@ def reboost_toots(mastodon_client: Mastodon, context: dict) -> None:
         print (f"url: {scored_post.url}")
         status = mastodon_client.status(scored_post.info['id'])
         #print (status.content)
-        print (f"Would call mastodon_client.reblog({scored_post.info['id']}, visibility='unlisted')")
+        print (f"Calling mastodon_client.reblog({scored_post.info['id']}, visibility='unlisted')")
+        mastodon_client.status_reblog(scored_post.info['id'], visibility='unlisted')
